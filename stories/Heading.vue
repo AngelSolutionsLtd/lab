ed83d3964fce1d63e612c2148cb916a6235ffc2d
Alex Dring <alex.dring@AngelSolutionsLtd.onmicrosoft.com>
2024-05-30 10:25:57 +0100

    added new component

---
+++ stories/Heading.vue
@@ -0,0 +1,19 @@
+<template>
+<div id="icons" class="grid">
+<i class="entypo--users"></i> <span class="i-name">entypo--users</span>
+     
+</div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Heading',
+    props: {
+      text: {
+        type: String,
+        required: true
+      }
+    }
+  }
+  </script>
+  
